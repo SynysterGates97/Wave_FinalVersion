9cdfe07c839c04f9116026ff9346d63c93d4136f
DESKTOP-GAHLO8A\Morgan <rusllann97@gmail.com>
2022-02-04 00:01:50 +0500

    Реализовал тестовый функционал для проверки того, можно ли в начале подключить BT-модуль в режиме команд, а затем в режиме данных.

--- Lib/bt_hc_05_driver.c
+++ Lib/bt_hc_05_driver.c
@@ -17,11 +17,15 @@
 #define BT_HC_05_GPIO_EN_PORT GPIOB
 #define BT_HC_05_GPIO_EN_PIN GPIO_PIN_1
 
+#define BT_HC_05_AT_MODE_UART_BAUD_RATE 38400
+
+#define BT_HC_05_DATA_MODE_UART_BAUD_RATE 57600
+
 #define BT_HC_05_SET_EN_PIN() \
-	HAL_GPIO_WritePin(BT_HC_05_GPIO_STATE_PORT, BT_HC_05_GPIO_EN_PIN, GPIO_PIN_SET)
+	HAL_GPIO_WritePin(BT_HC_05_GPIO_EN_PORT, BT_HC_05_GPIO_EN_PIN, GPIO_PIN_SET)
 
 #define BT_HC_05_RESET_EN_PIN() \
-	HAL_GPIO_WritePin(BT_HC_05_GPIO_STATE_PORT, BT_HC_05_GPIO_EN_PIN, GPIO_PIN_RESET)
+	HAL_GPIO_WritePin(BT_HC_05_GPIO_EN_PORT, BT_HC_05_GPIO_EN_PIN, GPIO_PIN_RESET)
 
 bool isAtModeEnabled = false;
 
@@ -43,14 +47,34 @@ void bt_hc_05_init(UART_HandleTypeDef *uartHandler, DMA_HandleTypeDef *dmaUartRx
 
 void bt_hc_05_switch_device_mode(bool isGoToAtMode)
 {
+	uint32_t newUartSpeed = 0;
 	if (isGoToAtMode)
 	{
+		newUartSpeed = BT_HC_05_AT_MODE_UART_BAUD_RATE;
+
 		BT_HC_05_SET_EN_PIN();
 	}
 	else
 	{
+		newUartSpeed = BT_HC_05_DATA_MODE_UART_BAUD_RATE;
+
 		BT_HC_05_RESET_EN_PIN();
+		HAL_StatusTypeDef transRes = HAL_UART_Transmit(btHc05Uart.uartHandler, (uint8_t*)"EN_RESAT+RESET\r\n", 10, 3000);
 	}
+
+	btHc05Uart.uartHandler->Init.BaudRate = newUartSpeed;
+	HAL_StatusTypeDef deinitResult = HAL_UART_DeInit(btHc05Uart.uartHandler);
+
+	if (deinitResult == HAL_OK)
+	{
+		HAL_StatusTypeDef initResult = HAL_UART_Init(btHc05Uart.uartHandler);
+		if (initResult == HAL_OK && isGoToAtMode)
+		{
+			HAL_StatusTypeDef transRes = HAL_UART_Transmit(btHc05Uart.uartHandler, (uint8_t*)"EN_TRUE_AT+RESET\r\n", 10, 3000);
+			int a  = 0;
+		}
+	}
+
 	isAtModeEnabled = isGoToAtMode;
 }
 
