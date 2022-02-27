49e9b0160a83dbe3bd45f0df908d75c0434d8373
DESKTOP-GAHLO8A\Morgan <rusllann97@gmail.com>
2022-02-27 15:54:26 +0500

    Revert "Написал функцию считывания байт по UART с использованием DMA"
    This reverts commit fdad0c634d758f84259bbaf4fef3d9eff36db9e5.

--- Lib/bt_hc_05_driver.c
+++ Lib/bt_hc_05_driver.c
@@ -8,9 +8,6 @@
 #include "bt_hc_05_driver.h"
 #include "stm32f407xx.h"
 
-// TX_MCU =
-
-#define BT_HC_05_RX_BUF_SIZE 256
 #define BT_HC_05_GPIO_STATE_PORT GPIOE
 #define BT_HC_05_GPIO_STATE_PIN GPIO_PIN_7
 
@@ -24,8 +21,6 @@
 	HAL_GPIO_WritePin(BT_HC_05_GPIO_STATE_PORT, BT_HC_05_GPIO_EN_PIN, GPIO_PIN_RESET)
 
 bool isAtModeEnabled = false;
-
-uint8_t btBuffer[BT_HC_05_RX_BUF_SIZE] = { 0 };
 
 static struct
 {
@@ -53,12 +48,10 @@ void bt_hc_05_switch_device_mode(bool isGoToAtMode)
 		BT_HC_05_RESET_EN_PIN();
 	}
 	isAtModeEnabled = isGoToAtMode;
+
 }
 
-void bt_hc_05_read_data()
-{
-	 HAL_UART_Receive_DMA(btHc05Uart.uartHandler, btBuffer, BT_HC_05_RX_BUF_SIZE);
-}
+
 
 
 
