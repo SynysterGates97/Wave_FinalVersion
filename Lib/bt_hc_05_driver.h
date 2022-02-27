45c9da32a1f6a317e2aee477979a15a2fcf78c14
DESKTOP-GAHLO8A\Morgan <rusllann97@gmail.com>
2022-02-27 15:54:42 +0500

    Revert "Исправил подключение библиотек HAL_GPIO - оказывается, нужно подключать только общую библиотеку."
    This reverts commit 94f88f50f8c1eacd7af1c5c5d47ab06e87697cf2.

--- Lib/bt_hc_05_driver.h
+++ Lib/bt_hc_05_driver.h
@@ -1,15 +1,14 @@
-/*
- * bt_hc_05_driver.h
- *
- *  Created on: Jan 28, 2022
- *      Author: Morgan
- */
-
-#ifndef BT_HC_05_DRIVER_H_
-#define BT_HC_05_DRIVER_H_
-
-#include "stdbool.h"
-
-void bt_hc_05_switch_device_mode(bool isGoToAtMode);
-
-#endif /* BT_HC_05_DRIVER_H_ */
+///*
+// * bt_hc_05_driver.h
+// *
+// *  Created on: Jan 28, 2022
+// *      Author: Morgan
+// */
+//#include "stdbool.h"
+//
+//#ifndef BT_HC_05_DRIVER_H_
+//#define BT_HC_05_DRIVER_H_
+//
+//void bt_hc_05_switch_device_mode(bool isGoToAtMode);
+//
+//#endif /* BT_HC_05_DRIVER_H_ */
